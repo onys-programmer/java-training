3f6c0b5c1d73843df79ce1d11c524a4b67e89436
thkim <thkim2017@gmail.com>
2021-01-02 19:06:44 +0900

    Make a solution to be passed Revise spacing issue

--- SpeedConverter/src/SpeedConverter.java
+++ SpeedConverter/src/SpeedConverter.java
@@ -1,6 +1,28 @@
 public class SpeedConverter {
-    public static void main(String[] args) {
 
+    public static long toMilesPerHour(double kilometersPerHour) {
 
+        if(kilometersPerHour >= 0) {
+            long calcKilometersToMiles = Math.round((kilometersPerHour/1.609));
+            return calcKilometersToMiles;
+        }
+
+        else {
+            return -1;
+        }
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+
+        if(kilometersPerHour >=0) {
+            int calcKilometersToMiles = (int) Math.round((kilometersPerHour/1.609));
+            String transformed = kilometersPerHour + " km/h = " + calcKilometersToMiles + " mi/h";
+            System.out.println(transformed);
+        }
+
+        else {
+            String errorMessage = "Invalid Value";
+            System.out.println(errorMessage);
+        }
     }
 }
