becc8b088e91830484dbcf5f0d7c4059d584bbbf
thkim <thkim2017@gmail.com>
2021-01-04 17:06:03 +0900

    Add setters Add setters for each field

--- BankAccount/src/BankAccount.java
+++ BankAccount/src/BankAccount.java
@@ -4,4 +4,24 @@ public class BankAccount {
     String customerName;
     String email;
     int phoneNumber;
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
