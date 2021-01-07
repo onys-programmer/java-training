4dcc9f83adf386f1f25662424b0f9be5f0ed5f64
thkim <thkim2017@gmail.com>
2021-01-07 19:01:49 +0900

    Try to make this solution be passed But failed

--- Point/src/Point.java
+++ Point/src/Point.java
@@ -1,2 +1,37 @@
 public class Point {
+    private int x;
+    private int y;
+
+    public Point() {
+        this(0, 0);
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y =y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y =y;
+    }
+
+    public double distance() {
+        return Math.sqrt(x^2 + y^2);
+    }
+
+    public double distance(int x, int y) {
+
+    }
 }
